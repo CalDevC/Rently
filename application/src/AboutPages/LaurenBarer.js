6253d5c9bec8db1b2504facf430d408229cda943
LaurenBarer-SFSU <70980931+LaurenBarer-SFSU@users.noreply.github.com>
2021-09-14 00:15:00 -0700

    LaurenBarer.js

---
+++ application/src/AboutPages/LaurenBarer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./profiles.module.css";
+
+function LaurenBarer(){
+    return(
+        <div>
+            <h1>Lauren Barer</h1>
+            <p className={classes.p}>Role: Scrum Master, Cloud /p>
+            <div className={classes.circleImgContainer}>
+                <img src={require("../images/Lauren.jpg").default} width={200} alt="" />
+            </div>
+        </div> 
+    );
+}
+
+export default LaurenBarer;
