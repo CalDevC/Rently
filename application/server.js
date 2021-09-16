6e38c8a3fc5fbf369ef41359c20b1492d6e575ed
chase <>
2021-09-16 13:32:20 -0700

    Added path variable

--- application/server.js
+++ application/server.js
@@ -1,9 +1,18 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
+const path = require("path");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.get('/*', function (req, res) {
+   	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 // This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET routes
 app.get('/api/chase-alexander', (req,res) => {
