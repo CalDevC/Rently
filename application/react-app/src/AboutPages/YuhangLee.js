f1ff1417c1867e5066b661ef19e6ae7cf81d3fa4
yuhang98 <lee.hng12@gmail.com>
2021-09-17 08:58:19 +0800

    Yuhang About Me Page Update

--- application/react-app/src/AboutPages/YuhangLee.js
+++ application/react-app/src/AboutPages/YuhangLee.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./profiles.module.css";
 
-function YuhangLee() {
-  return (
-    <div>
-      <h1 className={classes.h1}>Name : Yu Hang Lee</h1>
-      <p className={classes.p}>Role : Front End Lead</p>
-      <img src="images/Member1.jpg" width={200} alt="" />
-    </div>
-  );
+class YuhangLee extends Component {
+  state = {
+    profile: {},
+  };
+
+  componentDidMount() {
+    //retrieve profile
+    fetch("/api/yuhang-lee")
+      .then((res) => res.json())
+      .then((res) => this.setState({ profile: res }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.profile.name}</h1>
+        <p className={classes.p}>Role: {this.state.profile.role}</p>
+        <div className={classes.circleImgContainer}>
+          <img src={this.state.profile.img} width={200} alt="" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default YuhangLee;
