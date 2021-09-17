38726291603cff269b8a58235b3969529a144788
chuchengsitu <csitu1@mail.sfsu.edu>
2021-09-16 18:47:16 -0700

    Added Express to About Page

--- application/react-app/src/AboutPages/ChuChengSitu.js
+++ application/react-app/src/AboutPages/ChuChengSitu.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./profiles.module.css";
 
-function ChuChengSitu() {
-  return (
-    <div>
-      <h1 className={classes.h1}>Name : Chu Cheng Situ</h1>
-      <p className={classes.p}>Role : Backend Lead</p>
-      <img src="images/Chu.jpg" width={200} alt="" />
-    </div>
-  );
+class ChuChengSitu extends Component{
+  state = {
+    profile: {}
+  }
+
+  componentDidMount(){
+    //retrieve profile
+    fetch('/api/chase-alexander')
+    .then(res => res.json())
+    .then(res => this.setState({ profile: res }));
+  }
+
+  render(){
+    return(
+      <div>
+          <h1>{this.state.profile.name}</h1>
+          <p className={classes.p}>Role: {this.state.profile.role}</p>
+          <div>           
+            <img src={this.state.profile.img} width={200} alt="" />
+          </div>
+      </div>
+    );
+  }
 }
 
 export default ChuChengSitu;
