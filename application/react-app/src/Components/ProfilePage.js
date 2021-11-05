e0489f08b569b5cb48bd2264f32283ba60a90656
yuhang98 <lee.hng12@gmail.com>
2021-11-05 09:12:47 +0800

    Upload ProfilePage

--- application/react-app/src/Components/ProfilePage.js
+++ application/react-app/src/Components/ProfilePage.js
@@ -4,6 +4,11 @@ import React from "react";
 class ProfilePage extends Component {
   constructor() {
     super();
+    this.usernameInputRef = React.createRef();
+    this.passwordInputRef = React.createRef();
+    this.nameInputRef = React.createRef();
+    this.emailInputRef = React.createRef();
+
     this.submitHandler = this.submitHandler.bind(this);
     this.state = {
       data: {
@@ -22,8 +27,10 @@ class ProfilePage extends Component {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        username: this.usernameInputRef,
-        password: this.passwordInputRef,
+        username: this.usernameInputRef.value,
+        password: this.passwordInputRef.value,
+        name: this.nameInputRef.value,
+        email: this.emailInputRef.value,
       }),
     })
       .then((response) => response.json())
@@ -42,17 +49,46 @@ class ProfilePage extends Component {
         <h1>Rently</h1>
         <div>
           <label htmlFor="Username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            required
+            id="title"
+            ref={(node) => (this.usernameInputRef = node)}
+          />
+        </div>
+        <div>
+          <label htmlFor="Password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            required
+            id="title"
+            ref={(node) => (this.passwordInputRef = node)}
+          />
         </div>
         <div>
           <label htmlFor="Name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            required
+            id="title"
+            ref={(node) => (this.nameInputRef = node)}
+          />
         </div>
         <div>
           <label htmlFor="Email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            required
+            id="title"
+            ref={(node) => (this.emailInputRef = node)}
+          />
         </div>
         <label htmlFor="Picture">Picture</label>
-        <div>
-          <img src="images/Member1.jpg" width={200} alt="" />
-        </div>
+        <div>{/* <img src="images/Member1.jpg" width={200} alt="" /> */}</div>
         <div>
           <button>Save</button>
         </div>
