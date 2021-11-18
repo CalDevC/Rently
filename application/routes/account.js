5610dbe0b8f0f2dcbb76b0d290425c83a71949a7
chase <>
2021-11-18 11:52:04 -0800

    Fixed registration

--- application/routes/account.js
+++ application/routes/account.js
@@ -7,71 +7,73 @@ router.use(express.json());
 
 router.post('/login', (req, res) => {
 
-  const username = req.body.username;
-  const password = req.body.password;
+   const username = req.body.username;
+   const password = req.body.password;
 
-  if (!username || username.trim() === '' || !password || password.trim() === '') {
-    console.log('ERROR - no username or password given')
-    res.send('error');
-  }
+   if (!username || username.trim() === '' || !password || password.trim() === '') {
+      console.log('ERROR - no username or password given')
+      res.send('error');
+   }
 
-  let query = `SELECT * FROM Register_User WHERE username = '${username}'`;
+   let query = `SELECT * FROM Register_User WHERE username = '${username}';`;
 
-  //Make query for data
-  db.query(query)
-    .then(([results, fields]) => {
-      console.log(results);
+   //Make query for data
+   db.query(query)
+      .then(([results, fields]) => {
+         console.log(results);
 
-      //Send the data to the frontend
-      if (results.length > 0 && password == results[0].password) {
-        console.log(results);
-        res.send({
-          status: 'ok',
-          msg: 'Successfully logged in',
-          user: results[0]
-        });
-      }
-      else {
-        res.send({
-          status: 'bad',
-          msg: 'Invalid Username or Password'
-        });
-      }
+         //Send the data to the frontend
+         if (results.length > 0 && password == results[0].password) {
+            console.log(results);
+            res.send({
+               status: 'ok',
+               msg: 'Successfully logged in',
+               user: results[0]
+            });
+         }
+         else {
+            res.send({
+               status: 'bad',
+               msg: 'Invalid Username or Password'
+            });
+         }
 
-    })
-    .catch((err) => {
-      console.error('error connecting: ' + err.stack);
-      next(err);
-    })
+      })
+      .catch((err) => {
+         console.error('error connecting: ' + err.stack);
+         next(err);
+      })
 
 });
 
 
 router.post('/register', (req, res) => {
-  let username = req.body.username;
-  let email = req.body.email;
-  let password = req.body.password;
-  let dob = req.body.dob;
-  let address = req.body.address;
-  let zipCode = req.body.zipCode;
+   let username = req.body.username;
+   let email = req.body.email;
+   let password = req.body.password;
+   let dob = req.body.dob;
+   let address = req.body.address;
+   let zipCode = req.body.zipCode;
+   console.log(password);
+   console.log(dob);
 
-  let query = `INSERT INTO Register_User (username, email, password, dob, address, zipCode) values ('${username}', '${email}', '${password}', '${dob}', '${address}', '${zipCode}');`;
+   let query = `INSERT INTO Register_User (userName, email, salt, password, dob, address, zipCode) values ('${username}', '${email}', '', '${password}', '${dob}', '${address}', '${zipCode}');`;
 
-  //Make query for data
-  db.query(query)
-    .then(([results, fields]) => {
-      console.log(results);
+   //Make query for data
+   db.query(query)
+      .then(([results, fields]) => {
+         console.log(results);
 
-      //Send the data to the frontend
-      res.send({
-        status: 'ok',
-        msg: 'Successfully Register'
-      });
-    })
-    .catch((err) => {
-      console.error('error connecting: ' + err.stack);
-      //next(err);
-    })
+         //Send the data to the frontend
+         res.send({
+            status: 'ok',
+            msg: 'Successfully Register'
+         });
+      })
+      .catch((err) => {
+         console.error('error connecting: ' + err.stack);
+         //next(err);
+      })
 
 });
 
