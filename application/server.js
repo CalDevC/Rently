128f7d67671911c1828601388d77b507a64c09c4
chase <>
2021-12-14 13:52:44 -0800

    Images can be uploaded

--- application/server.js
+++ application/server.js
@@ -12,9 +12,9 @@ var db = require('./config/database');
 app.use(express.static(path.join(__dirname, "build")));
 
 if (process.env.NODE_ENV === 'production') {
-   app.get('/*', function (req, res) {
-      res.sendFile(path.join(__dirname, 'build', 'index.html'));
-   });
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 }
 
 // This displays message that the server running and listening to specified port
@@ -32,35 +32,35 @@ app.use('/api/posts', post);
 
 //Home Page Route
 app.get("/api/homepage", (req, res) => {
-   let query = 'SELECT Register_User.username, Register_User.email, \
+  let query = 'SELECT Register_User.username, Register_User.email, \
               Register_User.password, Register_User.dob, Register_User.address, \
               Register_User.zipCode FROM Rently.Register_User; \
               SELECT Equipment_Category.description \
               FROM Rently.Equipment_Category;';
 
-   //Make query for data
-   db.query(query)
-      .then(([results, fields]) => {
-         console.log(results);
+  //Make query for data
+  db.query(query)
+    .then(([results, fields]) => {
+      console.log(results);
 
-         //Send the data to the frontend
-         let userData = results[0][0];
-         let categoryData = results[1][0];
+      //Send the data to the frontend
+      let userData = results[0][0];
+      let categoryData = results[1][0];
 
-         res.send({
-            username: userData.username,
-            email: userData.email,
-            password: userData.password,
-            dob: userData.dob,
-            address: userData.address,
-            zipCode: userData.zipCode,
-            equipmentCategory: categoryData.description
-         });
-      })
-      .catch((err) => {
-         console.error('error connecting: ' + err.stack);
-         next(err);
-      })
+      res.send({
+        username: userData.username,
+        email: userData.email,
+        password: userData.password,
+        dob: userData.dob,
+        address: userData.address,
+        zipCode: userData.zipCode,
+        equipmentCategory: categoryData.description
+      });
+    })
+    .catch((err) => {
+      console.error('error connecting: ' + err.stack);
+      next(err);
+    })
 
 });
 
