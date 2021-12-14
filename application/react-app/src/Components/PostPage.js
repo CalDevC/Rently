5dd663da27b5bb6d8e19eddbccad9d48a39aff20
chase <>
2021-12-14 15:03:48 -0800

    Code clean up and image thumbnails

--- application/react-app/src/Components/PostPage.js
+++ application/react-app/src/Components/PostPage.js
@@ -7,19 +7,7 @@ import styles from '../CSS/Post.module.css';
 class PostPage extends Component {
   constructor() {
     super();
-
-    this.equipmentInputRef = React.createRef();
-    this.priceInputRef = React.createRef();
-    this.depositFeeInputRef = React.createRef();
-    this.locationInputRef = React.createRef();
-    this.deliveryInputRef = React.createRef();
-    this.descriptionInputRef = React.createRef();
     this.categoryInputRef = React.createRef();
-    this.penaltyFeeInputRef = React.createRef();
-    this.startInputRef = React.createRef();
-    this.endInputRef = React.createRef();
-    this.imageInputRef = React.createRef();
-
     this.submitHandler = this.submitHandler.bind(this);
     this.inputChangeHandler = this.inputChangeHandler.bind(this);
     this.routeChange = this.routeChange.bind(this);
@@ -60,21 +48,6 @@ class PostPage extends Component {
     await fetch('/api/posts/addPost', {
       method: 'POST',
       body: formData
-      // body: JSON.stringify({
-      //   user: localStorage.getItem('user'),
-      //   category: this.categoryInputRef.state.value,
-      //   equipment: this.equipmentInputRef.value,
-      //   price: this.priceInputRef.value,
-      //   depositFee: this.depositFeeInputRef.value,
-      //   penalty: this.penaltyFeeInputRef.value,
-      //   location: this.locationInputRef.value,
-      //   delivery: this.deliveryInputRef.checked,
-      //   description: this.descriptionInputRef.value,
-      //   start: this.startInputRef.value,
-      //   end: this.endInputRef.value,
-      //   image: this.imageInputRef.value,
-      //   data: formData
-      // }),
     })
       .then((response) => response.text())
       .then((res) => {
@@ -110,8 +83,7 @@ class PostPage extends Component {
 
           <div className={styles.username}>
             <label htmlFor="Equipment">Equipment Name:</label>
-            <input type="text" id="equipment" onChange={this.inputChangeHandler} ref={(node) => (this.equipmentInputRef = node)}
-            />
+            <input type="text" id="equipment" onChange={this.inputChangeHandler} />
           </div>
 
           <div className={styles.username}>
@@ -119,49 +91,48 @@ class PostPage extends Component {
           </div>
 
           <div className={styles.username}>
-            <input type="file" accept="image/*" id="image" onChange={this.fileSelected} ref={(node) => (this.imageInputRef = node)} />
+            <input type="file" accept="image/*" id="image" onChange={this.fileSelected} />
           </div>
 
           <div className={styles.username}>
             <label htmlFor="Price">Rental Price $</label>
-            <input type="text" id="price" onChange={this.inputChangeHandler} ref={(node) => (this.priceInputRef = node)} />
+            <input type="text" id="price" onChange={this.inputChangeHandler} />
           </div>
 
           <div className={styles.username}>
             <label htmlFor="Deposit">Security Deposit $</label>
-            <input type="text" id="deposit" onChange={this.inputChangeHandler} ref={(node) => (this.depositFeeInputRef = node)} />
+            <input type="text" id="deposit" onChange={this.inputChangeHandler} />
           </div>
 
           <div className={styles.username}>
             <label htmlFor="Penalty">Penalty for damages $</label>
-            <input type="text" id="penalty" onChange={this.inputChangeHandler} ref={(node) => (this.penaltyFeeInputRef = node)} />
+            <input type="text" id="penalty" onChange={this.inputChangeHandler} />
           </div>
 
           <div className={styles.username}>
             <label htmlFor="Location">Pick-up Location:</label>
-            <input type="text" id="location" onChange={this.inputChangeHandler} ref={(node) => (this.locationInputRef = node)}
-            />
+            <input type="text" id="location" onChange={this.inputChangeHandler} />
           </div>
 
           <div className={styles.username}>
             <label htmlFor="Delivery">Offer Delivery?</label>
-            <input type="checkbox" id="delivery" onChange={this.inputChangeHandler} ref={(node) => (this.deliveryInputRef = node)} />
+            <input type="checkbox" id="delivery" onChange={this.inputChangeHandler} />
           </div>
 
           <div className={styles.username}>
             <label htmlFor="Description">Description:</label>
-            <input type="text" id="description" onChange={this.inputChangeHandler} ref={(node) => (this.descriptionInputRef = node)} />
+            <input type="text" id="description" onChange={this.inputChangeHandler} />
           </div>
 
           <div className={styles.username}>
             <h4>Rental Times</h4>
             <label htmlFor="start">Start Date:</label>
-            <input type="text" id="startDate" onChange={this.inputChangeHandler} ref={(node) => (this.startInputRef = node)} />
+            <input type="text" id="startDate" onChange={this.inputChangeHandler} />
           </div>
 
           <div className={styles.username}>
             <label htmlFor="end">End Date:</label>
-            <input type="text" id="endDate" onChange={this.inputChangeHandler} ref={(node) => (this.endInputRef = node)} />
+            <input type="text" id="endDate" onChange={this.inputChangeHandler} />
           </div>
 
           <div>
